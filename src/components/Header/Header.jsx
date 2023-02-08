6d6b795b09412c22b87a3242ae33b12915bcaa20
gualin <iiwwpr1@gmail.com>
2023-02-08 22:32:09 +0800

    chore:新增/修改 css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,13 +7,13 @@ const TAB = [
   { label: "Skills", path: "" },
   { label: "Portfolios", path: "" },
   { label: "Contact", path: "" },
-  { label: "Signin", path: "" },
+  { label: "Sign in", path: "" },
 ];
 const tabStyle =
   "mx-[5px] text-lg cursor-pointer transition duration-300 hover:scale-110 py-[5px] px-[15px]";
 const Header = () => {
   return (
-    <header className="w-full h-[70px] bg-[black] text-white p-5 fixed top-0 left-0">
+    <header className="w-full h-[70px] bg-[#242424] text-white p-5 fixed top-0 left-0 z-[9999]">
       <section className="mx-auto w-[70%] flex justify-between items-center">
         <section>
           <p className="font-semibold text-xl">My Name</p>
@@ -21,9 +21,9 @@ const Header = () => {
         <section className="flex">
           {TAB.map((tab) => (
             <Link
-              to={`/${tab.label.toLocaleLowerCase()}`}
+              to={`/${tab.label.toLocaleLowerCase().replace(/\s/g, "")}`}
               className={`${tabStyle} ${
-                tab.label === "Signin" &&
+                tab.label === "Sign in" &&
                 "rounded-full bg-purple-400 !text-white font-bold hover:bg-purple-600"
               }`}
             >
