570ece37f0a7990b0ebf2c0c929846b8a270a10f
gualin <iiwwpr1@gmail.com>
2023-02-08 22:31:41 +0800

    feat:新增密碼、電子郵件字數限制

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,3 +1,7 @@
 export const API_URL = `${
   import.meta.env.VITE_API_URL
 }/Myproject1/func/member.php`;
+
+export const PASSWORD_EXPRESS = /^[^\W_]{6,12}$/;
+export const EMAIL_EXPRESS =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
