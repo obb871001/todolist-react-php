dc249d4d30e4da90d73a257aed69e7217b7b10da
gualin <iiwwpr1@gmail.com>
2023-02-12 22:26:15 +0800

    feat:新增登入驗證api

--- src/api/apis.js
+++ src/api/apis.js
@@ -6,15 +6,22 @@ export const SignInWeb = ({ email, password } = {}) => {
     method: "login",
     email: email,
     password: password,
-  })
+  });
 };
 
-export const SignUpWeb = ({ email, password,userName,password2 } = {}) => {
-    return axios.post(`${API_URL}`, {
-      method: "Register",
-      userName: userName,
-      password: password,
-      password2:password2,
-      email:email,
-    })
-  };
+export const CheckOauth = () => {
+  return axios.post(`${API_URL}`, {
+    method: "check_session",
+    oauth: sessionStorage.getItem("sess_oauth"),
+  });
+};
+
+export const SignUpWeb = ({ email, password, userName, password2 } = {}) => {
+  return axios.post(`${API_URL}`, {
+    method: "Register",
+    userName: userName,
+    password: password,
+    password2: password2,
+    email: email,
+  });
+};
